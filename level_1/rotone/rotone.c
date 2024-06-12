69a6b06dde9dfc66cde0f2aaf43784ace97f5ebc
achivela <achivela@student.42luanda.com>
2024-06-12 10:57:50 +0100

    first level (search_and_replace)

--- level_1/rotone/rotone.c
+++ level_1/rotone/rotone.c
@@ -7,27 +7,16 @@ void rotone(char *str)
 	while(str[i] != '\0')
 	{
 		if(str[i] >= 'a' && str[i] <= 'y')
-		{
-			str[i] += 1;
-			write(1,&str[i],1);
-		}
-		else if(str[i] >= 'A' && str[i] <= 'y')
-		{
 			str[i] +=1;
-			write(1,&str[i],1);
-		}
+		else if (str[i] >= 'A' && str[i] <= 'Y')
+			str[i] +=1;
 		else if(str[i] == 'z' || str[i] == 'Z')
-		{
-			str[i] -= 25;
-			write(1,&str[i],1);
-		}else
-		{
+			str[i]-=25;
 		write(1,&str[i],1);
-		}
-		
 		i++;
-	}
+	 }
 }
+
 int main(int argc,char **argv)
 {
 	if(argc == 2)
