7dcf4eac434b0285136b037138d5e3c50c5fe2b5
achivela <achivela@student.42luanda.com>
2024-06-12 13:22:40 +0100

    second level (ft_atoi)

---
+++ level_2/ft_atoi/ft_atoi.c
@@ -0,0 +1,22 @@
+#include<unistd.h>
+
+int ft_atoi(const char *str)
+{
+	int i;
+	int result = 0;
+	int sign = 1;
+	
+	i = 0;
+	while(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if(str[i] == '-')
+		sign = -1;
+	if(str[i] == '-' || str[i] == '+')
+		i++;
+	while(str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}	
+	return (sign * result);
+}
