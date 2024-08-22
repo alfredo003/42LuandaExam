f6638552584f350c9e0cca5ed57d9fe9569e449b
Alfredo Manuel <59560615+alfredo003@users.noreply.github.com>
2024-08-22 15:19:12 +0000

    updated

---
+++ level_4/fprime/fprime.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void fprime(int num)
+{
+    int i = 1;
+    if(num == 1)
+        printf("1");
+    while(num >= ++i)
+    {
+           if(num % i == 0)
+           {
+                printf("%d",i);
+                if(num == i)
+                    break;
+                printf("*");
+                num /= i;
+                i=1;
+           }
+    }
+}
+
+int main(int argc,char **argv)
+{
+    if(argc == 2)
+    {
+        int num = atoi(argv[1]);
+        fprime(num);
+    }
+    printf("\n");
+    return (0);
+}
