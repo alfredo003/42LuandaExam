4be2cb4ec21869fe7bffa385ac766a7920f49676
Alfredo Manuel <59560615+alfredo003@users.noreply.github.com>
2024-08-23 08:16:27 +0000

    (level_3):expand_str

--- level_2/ft_atoi/ft_atoi.c
+++ level_2/ft_atoi/ft_atoi.c
@@ -1,20 +1,17 @@
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-	int  i = 0;
-	int sign = 1;
-	int result = 0;
-	
-	while(str[i] == ' ' || str[i] == '\t')
-		i++;
-	if(str[i] == '-')
-		sign=-1;
-	if(str[i] == '-' || str[i] == '+')
-		i++;
-	while(str[i] >= '0' && str[i] <= '9')
+	int sign =1;
+	int res =0;
+
+	while(*str == ' ' || *str == '\t')
+		str++;
+	if(*str == '-')
+		sign *=-1;
+	if(*str == '-' || *str == '+')
+		str++;
+	while(*str >= '0' && *str <= '9')
 	{
-		res = res * 10 + str[i] - 48;
-		i++;
+		res = res * 10 + *str++ - 48;
 	}
-	
-	return (res * sign);
+	return (sign * res);
 }
